1251b04b1775f6566f726ac6e96aea8ed32c0e43
Nikita <belka.nik02@gmail.com>
2026-01-17 16:02:38 +0200

    404 page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+import styles from './NotFoundPage.module.scss';
+
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>NotFoundPage</h1>
+    <div className={styles.notFoundPage}>
+      <h1>404</h1>
+      <h2>Not Found Page</h2>
+      <Link to='/' className={styles.buttonLink}>
+        <p>на главную</p>
+      </Link>
     </div>
   );
 }
