80e3d40df484172dc3647e7e9d8c3c8ed5d7b998
Nikita <belka.nik02@gmail.com>
2026-03-02 14:25:25 +0200

    test 3

---
+++ src/api/prerender.js
@@ -0,0 +1,35 @@
+export default async function handler(req, res) {
+  const userAgent = req.headers['user-agent'] || '';
+
+  const bots = [
+    'googlebot',
+    'bingbot',
+    'yandex',
+    'twitterbot',
+    'facebookexternalhit',
+    'linkedinbot',
+  ];
+
+  const isBot = bots.some((bot) => userAgent.toLowerCase().includes(bot));
+
+  if (!isBot) {
+    return res.status(404).end();
+  }
+
+  const prerenderUrl = `https://service.prerender.io/${req.headers.host}${req.url}`;
+
+  try {
+    const response = await fetch(prerenderUrl, {
+      headers: {
+        'X-Prerender-Token': process.env.PRERENDER_TOKEN,
+      },
+    });
+
+    const html = await response.text();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send(html);
+  } catch (error) {
+    res.status(500).send('Prerender error');
+  }
+}
