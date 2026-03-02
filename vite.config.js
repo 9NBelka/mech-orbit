e49c3d823b7aa43d7a66eb8a57a4d702a707b6fd
Nikita <belka.nik02@gmail.com>
2026-03-02 16:33:14 +0200

    backup

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import prerender from 'vite-plugin-prerender';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      // Путь к страницам, которые хотим пререндерить
-      staticDir: 'dist',
-      routes: [
-        '/',
-        '/login',
-        '/register',
-        '/privacy-policy',
-        '/public-offer',
-        '/terms-of-use',
-        '/ru',
-        '/en',
-        '/ua',
-      ], // ваши маршруты
-    }),
-  ],
+  plugins: [react()],
 });
