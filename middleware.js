ecf01d30273705838d364c85e99bbddc8fd7aed9
Nikita <belka.nik02@gmail.com>
2026-03-02 19:07:15 +0200

    test 2

--- middleware.js
+++ middleware.js
@@ -1,5 +1,4 @@
-import { NextResponse } from 'next/server';
-
+// Список ботов
 const BOT_AGENTS = [
   'googlebot',
   'bingbot',
@@ -31,12 +30,12 @@ const BOT_AGENTS = [
   'Qwantify',
 ];
 
-export function middleware(request) {
+export default async function middleware(request) {
   const userAgent = request.headers.get('user-agent')?.toLowerCase();
   const url = new URL(request.url);
   const token = process.env.PRERENDER_TOKEN;
 
-  // Если зашел бот и это не запрос к статичному файлу (с расширением .js, .css, .png и т.д.)
+  // Проверяем: это бот и это не запрос за файлом (картинкой, скриптом)
   if (
     token &&
     userAgent &&
@@ -45,12 +44,22 @@ export function middleware(request) {
   ) {
     const renderUrl = `https://service.prerender.io{url.href}`;
 
-    return fetch(renderUrl, {
+    // Запрашиваем готовую страницу у Prerender.io
+    const res = await fetch(renderUrl, {
       headers: {
         'X-Prerender-Token': token,
       },
     });
+
+    // Возвращаем результат роботу
+    return new Response(res.body, res);
   }
 
-  return NextResponse.next();
+  // Если это обычный юзер — просто идем дальше
+  return fetch(request);
 }
+
+// Указываем Vercel, на какие пути реагировать (обычно на все)
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+};
